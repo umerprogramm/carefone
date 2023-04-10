2f30e45ce01e972d850b7d159cf104c9cf6d2330
Your Name <you@example.com>
2023-04-10 21:31:06 +0500

    commit 1

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,15 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest } from 'next'
 import { connectToDatabase } from '../../utils/mongodb';
 
 
-type Data = {
-  name: string
-}
+
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: 
 ) {
   const { db } = await connectToDatabase();
   const users = await db.collection('products').find().toArray();
